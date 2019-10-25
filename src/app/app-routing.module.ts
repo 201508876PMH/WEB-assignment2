7661c84fbb925adae95b0755062fcdd9891c4b3c
Oliver Monberg <olivermonberg@gmail.com>
2019-10-25 10:58:00 +0200

    home ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { WorkoutProgramsComponent } from './workout-programs/workout-programs.component';
 
 
 const routes: Routes = [
   { path: 'createAccount', component: CreateAccountComponent },  
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'workoutPrograms', component: WorkoutProgramsComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LoginComponent } from './login/login.component';
+import { WorkoutProgramsComponent } from './workout-programs/workout-programs.component';
+import { HomeComponent } from './home/home.component';
+import { CarouselComponent } from './home/carousel/carousel.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateAccountComponent,
     HeaderComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent,
+    WorkoutProgramsComponent,
+    HomeComponent,
+    CarouselComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
