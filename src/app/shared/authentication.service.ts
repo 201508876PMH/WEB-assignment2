86eec53b26e9bdcbbfca16c1210ae65f08b58553
andreas <androgles1994@gmail.com>
2019-10-27 16:16:47 +0100

    auth service

---
+++ src/app/shared/authentication.service.ts
@@ -0,0 +1,100 @@
+import {Injectable} from '@angular/core';
+import {User} from '../models/user.model';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  baseUrl = 'https://protected-eyrie-63584.herokuapp.com/api/users';
+  redirectUrl: string;
+
+  constructor(private httpClient: HttpClient,
+              private router: Router) {
+  }
+
+  public getCurrentUser() {
+    if (this.isLoggedIn()) {
+      const token = this.getToken();
+      const payload = JSON.parse(window.atob(token.split('.')[1]));
+      const user = new User();
+      user.emailAddress = payload.emailAddress;
+      user.firstName = payload.firstName;
+      user.lastName = payload.lastName;
+      return user;
+    } else {
+      return '';
+    }
+  }
+
+  public isLoggedIn() {
+    const token = this.getToken();
+
+    if (token) {
+      const payload = JSON.parse(window.atob(token.split('.')[1]));
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  }
+
+  public logout() {
+    this.removeToken();
+  }
+
+  public login(user: User) {
+    console.log('logging in this user:', user);
+    const url = `${this.baseUrl}/login`;
+    this.httpClient.post(url, user).subscribe(data => {
+      this.saveToken(data.token);
+      console.log(this.redirectUrl);
+
+      if (this.redirectUrl) {
+        console.log(this.redirectUrl);
+        this.router.navigate([this.redirectUrl]);
+      } else {
+        this.router.navigate(['/']);
+      }
+      return true;
+    }, (err: HttpErrorResponse) => {
+      if (err.error instanceof Error) {
+        console.log('an error occured:', err.error.message);
+      } else {
+        console.log(`backend return code ${err.status}, body was: ${err.error}`);
+      }
+    });
+
+  }
+
+  public register(user: User) {
+    const url = `${this.baseUrl}/createUser`;
+    this.httpClient.post(url, user).subscribe(data => {
+        // this.saveToken(data.token);
+        return true;
+      }, (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('an error occured:', err.error.message);
+        } else {
+          console.log(`backend return code ${err.status}, body was: ${err.error}`);
+        }
+      }
+    );
+  }
+
+  private saveToken(token: string) {
+    window.localStorage['loc8r-token'] = token;
+  }
+
+  private removeToken() {
+    window.localStorage.removeItem('loc8r-token');
+  }
+
+  private getToken() {
+    if (window.localStorage['loc8r-token']) {
+      return window.localStorage['loc8r-token'];
+    } else {
+      return '';
+    }
+  }
+}
---
+++ src/app/workout-programs/workout-program.service.ts
@@ -0,0 +1,22 @@
+import {WorkoutProgram} from '../models/workoutProgram.model';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorkoutProgramService {
+
+  workoutProgramsUrl = 'https://protected-eyrie-63584.herokuapp.com/api/workoutPrograms/';
+  workoutPrograms: WorkoutProgram[];
+
+  constructor(private httpClient: HttpClient) {
+    this.getWorkoutPrograms();
+  }
+
+  public getWorkoutPrograms(): Observable<WorkoutProgram[]> {
+
+    return this.httpClient.get<WorkoutProgram[]>(this.workoutProgramsUrl + 'getWorkoutPrograms');
+  }
+}
